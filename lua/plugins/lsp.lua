2ec00bb2d6688574a71bd85cae4ffa1f94b0d30f
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-11-07 11:45:42 -0500

    Addded rust

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,7 +36,48 @@ return {
 
 
         -- Rust
-        { 'simrat39/rust-tools.nvim' },
+        {
+            'simrat39/rust-tools.nvim',
+                tools = {
+                    inlay_hints = {
+                        -- automatically set inlay hints (type hints)
+                        -- default: true
+                        auto = true,
+
+                        -- Only show inlay hints for the current line
+                        only_current_line = true,
+
+                        -- whether to show parameter hints with the inlay hints or not
+                        -- default: true
+                        show_parameter_hints = true,
+
+                        -- prefix for parameter hints
+                        -- default: "<-"
+                        parameter_hints_prefix = "<- ",
+
+                        -- prefix for all the other hints (type, chaining)
+                        -- default: "=>"
+                        other_hints_prefix = "=> ",
+
+                        -- whether to align to the length of the longest line in the file
+                        max_len_align = false,
+
+                        -- padding from the left if max_len_align is true
+                        max_len_align_padding = 1,
+
+                        -- whether to align to the extreme right or not
+                        right_align = false,
+
+                        -- padding from the right if right_align is true
+                        right_align_padding = 7,
+
+                        -- The color of the hints
+                        highlight = "Comment",
+                    },
+                }
+            }
+
+        },
         {
             'saecki/crates.nvim',
             tag = 'v0.4.0',
@@ -74,7 +115,7 @@ return {
             'pyright',
             'nil_ls',
             -- 'gopls',
-            'gramerly'
+            -- 'gramerly'
         }
 
         vim.api.nvim_create_user_command("Format", function(args)
