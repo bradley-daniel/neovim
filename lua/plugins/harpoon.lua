92fd78e810fb9cf1574af72387ac4c3f683b8037
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-11-07 11:49:31 -0500

    Fixed harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,12 +9,12 @@ return {
         local ui = require("harpoon.ui")
 
         vim.keymap.set("n", "<leader>m", mark.add_file, { desc = "Harpoon mark" })
-        vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu, { esc = "Harpoon Menu"})
+        vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu, { desc = "Harpoon Menu"})
 
         vim.keymap.set("n", "ma", function() ui.nav_file(1) end, { desc = "Harpoon nav_file 1" })
         vim.keymap.set("n", "ms", function() ui.nav_file(2) end, { desc = "Harpoon nav_file 2" })
         vim.keymap.set("n", "md", function() ui.nav_file(3) end, { desc = "Harpoon nav_file 3" })
-        vim.keymap.set("n", "<mf>", function() ui.nav_file(4) end, { desc = "Harpoon nav_file 4" })
+        vim.keymap.set("n", "mf", function() ui.nav_file(4) end, { desc = "Harpoon nav_file 4" })
     end,
 
 }
