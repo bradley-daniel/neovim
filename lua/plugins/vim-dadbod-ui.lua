50b8e97d0523af952593ef7bc913e6e879c3b0b2
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-11-11 12:47:06 -0500

    updated plugin config

--- lua/plugins/vim-dadbod-ui.lua
+++ lua/plugins/vim-dadbod-ui.lua
@@ -14,6 +14,9 @@ return {
     },
     init = function()
         -- vim.g.db_ui_disable_mappings = 1
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.db_ui_execute_on_save = 0
+
 
 
 
@@ -29,6 +32,5 @@ return {
             fg = '#8aadf4',
         })
 
-        vim.g.db_ui_use_nerd_fonts = 1
     end,
 }
