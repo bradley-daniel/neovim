3b81549b913e9a16795666bdca3171a9516c3117
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-11-15 03:31:31 -0500

    Updated python linter

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -5,8 +5,8 @@ return {
         local null_ls = require('null-ls')
         require('null-ls').setup({
             sources = {
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.diagnostics.flake8,
+                -- null_ls.builtins.formatting.black,
+                null_ls.builtins.diagnostics.ruff,
             },
         })
     end,
