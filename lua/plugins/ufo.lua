7502f787d61ac8eee245cf8edf586171b5f85440
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-12-03 14:36:10 -0500

    add

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -2,7 +2,8 @@ return {
     'kevinhwang91/nvim-ufo',
     dependencies = {
         'kevinhwang91/promise-async',
-        'nvim-treesitter/nvim-treesitter'
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-treesitter/queries/filetype/folds.scm'
     },
     config = function()
         -- require('ufo').setup({
@@ -11,13 +12,21 @@ return {
         --     end,
         --
         -- })
+        --
+
+        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+        require('ufo').setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { 'treesitter', 'indent' }
+            end
+        })
         require('ufo').setup()
     end,
     intit = function()
         vim.o.foldcolumn = '1' -- '0' is not bad
         vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
+        vim.o.foldenable = false
 
         -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
         vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
