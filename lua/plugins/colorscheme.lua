cc5a14777cee8eed14bf0e7fa365941d6e014a3f
danielbradley.2020@gmail.com <danielbradley.2020@gmail.com>
2023-12-09 02:00:23 -0500

    updated fold bg color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,47 +1,52 @@
 return {
-    "catppuccin/nvim",
-    enabled = true,
-    priority = 1000,
-    lazy = false,
-    config = function()
-        local opts = {
-            flavour = "mocha", -- latte, frappe, macchiato, mocha
-            term_color = true,
-            color_overrides = {
-                mocha = {
-                    base = "#14191e",
-                    mantle = "#14191e",
-                    crust = "#bac2de",
+    {
+        "catppuccin/nvim",
+        enabled = true,
+        priority = 1000,
+        lazy = false,
+        config = function()
+            local opts = {
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                term_color = true,
+                color_overrides = {
+                    mocha = {
+                        base = "#14191e",
+                        mantle = "#14191e",
+                        crust = "#bac2de",
+                    },
                 },
-            },
-            custom_highlights = function(colors)
-                return {
-                    ['@function.builtin'] = { fg = colors.blue },
-                }
-            end
-            -- higlight_overrides = {
-            --     all = function(colors)
-            --         return {
-            --             Comment = { fg = "#000000" },
-            --         }
-            --     end,
-            -- },
-            --     transparent_background = true,
-            --     custom_highlights = function(colors)
-            --         local u = require("catppuccin.utils.colors")
-            --         return {
-            --             CursorLine = {
-            --                 bg = u.vary_color(
-            --                     {
-            --                         latte = u.lighten(colors.mantle, 0.70, colors.base)
-            --                     },
-            --                     u.darken(colors.surface0, 0.64, colors.base)
-            --                 ),
-            --             },
-            --         }
-            --     end,
-        }
-        require('catppuccin').setup(opts)
-        vim.cmd [[ colorscheme catppuccin ]]
-    end,
+                custom_highlights = function(colors)
+                    return {
+                        ['@function.builtin'] = { fg = colors.blue },
+                    }
+                end,
+                -- higlight_overrides = {
+                --     all = function(colors)
+                --         return {
+                --             Comment = { fg = "#000000" },
+                --         }
+                --     end,
+                -- },
+                --     transparent_background = true,
+                --     custom_highlights = function(colors)
+                --         local u = require("catppuccin.utils.colors")
+                --         return {
+                --             CursorLine = {
+                --                 bg = u.vary_color(
+                --                     {
+                --                         latte = u.lighten(colors.mantle, 0.70, colors.base)
+                --                     },
+                --                     u.darken(colors.surface0, 0.64, colors.base)
+                --                 ),
+                --             },
+                --         }
+                --     end,
+            }
+            require('catppuccin').setup(opts)
+            vim.cmd [[ colorscheme catppuccin ]]
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim"
+    }
 }
