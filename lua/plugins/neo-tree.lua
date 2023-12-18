ca7a34c479c04005b5e821dea856ab6505a770aa
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-12-18 03:22:14 -0500

    new commit

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    -- cmd = "Neotree",
+    cmd = "Neotree",
     -- ft = 'netrw',
     branch = "v3.x",
     dependencies = {
@@ -29,13 +29,6 @@ return {
             open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
             sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
             sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
-            -- sort_function = function (a,b)
-            --       if a.type == b.type then
-            --           return a.path > b.path
-            --       else
-            --           return a.type > b.type
-            --       end
-            --   end , -- this sorts files and directories descendantly
             default_component_configs = {
                 container = {
                     enable_character_fade = true
