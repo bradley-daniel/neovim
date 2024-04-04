3ece08cd68b6ccb6ce636b6b44f1b383a6c3524a
danielbradley.2020@gmail.com <danielbradley.2020@gmail.com>
2024-04-04 00:13:08 -0400

    updated

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,66 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-    priority = 1,
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        'nvim-treesitter/playground',
-    },
-    config = function()
-        -- -@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup {
-            -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = {
-                -- Neovim
-                'vimdoc',
-                'vim',
-                'lua',
-                -- Programing lanugages
-                'c',
-                'cpp',
-                'rust',
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
+	priority = 1,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"nvim-treesitter/playground",
+	},
+	config = function()
+		-- -@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			-- Add languages to be installed here that you want installed for treesitter
+			ensure_installed = {
+				-- Neovim
+				"vimdoc",
+				"vim",
+				"lua",
+				-- Programing lanugages
+				"c",
+				"cpp",
+				"rust",
 
-                'go',
-                'python',
-                'typescript',
-                'javascript',
+				"go",
+				"python",
+				"typescript",
+				"javascript",
 
-                'bash',
-                'nix',
-                --
-                -- -- Other
-                'html',
-                'json',
-                'toml',
-                -- 'yaml',
-                'htmldjango',
-                --
-                -- -- Quarto / markdown
-                'markdown',
-                'markdown_inline',
-                --
-                -- -- Graphing in quarto
-                'dot',
-                'mermaid',
+				"bash",
+				"nix",
+				--
+				-- -- Other
+				"html",
+				"json",
+				"toml",
+				-- 'yaml',
+				"htmldjango",
+				--
+				-- -- Quarto / markdown
+				"markdown",
+				"markdown_inline",
+				--
+				-- -- Graphing in quarto
+				"dot",
+				"mermaid",
 
-                -- Database
-                'sql',
-            },
+				-- Database
+				"sql",
+			},
 
+			autotag = { enable = true },
 
-            -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-            auto_install = false,
-            sync_install = false,
-            ignore_install = {},
-            modules = {},
+			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+			auto_install = false,
+			sync_install = false,
+			ignore_install = {},
+			modules = {},
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            },
-            indent = { enable = true },
-        }
-    end,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+		})
+	end,
 }
