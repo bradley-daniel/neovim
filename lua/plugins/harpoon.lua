4527c3242454729fdab0df54a00a0a51c4dfa9e1
danielbradley.2020@gmail.com <danielbradley.2020@gmail.com>
2024-04-21 23:56:10 -0400

    feat: update keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,13 +8,22 @@ return {
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
-        vim.keymap.set("n", "<leader>m", mark.add_file, { desc = "Harpoon mark" })
-        vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu, { desc = "Harpoon Menu"})
 
-        vim.keymap.set("n", "ma", function() ui.nav_file(1) end, { desc = "Harpoon nav_file 1" })
-        vim.keymap.set("n", "ms", function() ui.nav_file(2) end, { desc = "Harpoon nav_file 2" })
-        vim.keymap.set("n", "md", function() ui.nav_file(3) end, { desc = "Harpoon nav_file 3" })
-        vim.keymap.set("n", "mf", function() ui.nav_file(4) end, { desc = "Harpoon nav_file 4" })
+        -- vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        --
+        -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+        -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+        -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+        vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon mark" })
+        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon Menu"})
+        --
+        vim.keymap.set("n", "<C-a>", function() ui.nav_file(1) end, { desc = "Harpoon nav_file 1" })
+        vim.keymap.set("n", "<C-s>", function() ui.nav_file(2) end, { desc = "Harpoon nav_file 2" })
+        vim.keymap.set("n", "<C-d>", function() ui.nav_file(3) end, { desc = "Harpoon nav_file 3" })
+        vim.keymap.set("n", "<C-f>", function() ui.nav_file(4) end, { desc = "Harpoon nav_file 4" })
     end,
 
 }
