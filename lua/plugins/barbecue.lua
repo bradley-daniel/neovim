92c0fd3242b98892d2e75a6521475df7c48698df
danielbradley.2020@gmail.com <danielbradley.2020@gmail.com>
2024-04-22 00:18:34 -0400

    feat: udpate lazy loading

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,11 +1,12 @@
 return {
-  'utilyre/barbecue.nvim',
-  dependencies = {
-    'SmiteshP/nvim-navic',
-    'nvim-tree/nvim-web-devicons', -- optional dependency
-  },
-  config = function()
-    require('barbecue').setup {}
-    require('barbecue.ui').toggle(true)
-  end,
+	"utilyre/barbecue.nvim",
+	event = { "VeryLazy" },
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	config = function()
+		require("barbecue").setup({})
+		require("barbecue.ui").toggle(true)
+	end,
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,53 +1,53 @@
 return {
-    {
-        "catppuccin/nvim",
-        enabled = true,
-        priority = 1000,
-        lazy = false,
-        config = function()
-            local opts = {
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
-                term_color = true,
-                color_overrides = {
-                    mocha = {
-                        base = "#080A0F",
-                        mantle = "#080A0F",
-                        crust = "#bac2de",
-                    },
-                },
-                custom_highlights = function(colors)
-                    return {
-                        ['@function.builtin'] = { fg = colors.blue },
-                    }
-                end,
-                -- higlight_overrides = {
-                --     all = function(colors)
-                --         return {
-                --             Comment = { fg = "#000000" },
-                --         }
-                --     end,
-                -- },
-                --     transparent_background = true,
-                --     custom_highlights = function(colors)
-                --         local u = require("catppuccin.utils.colors")
-                --         return {
-                --             CursorLine = {
-                --                 bg = u.vary_color(
-                --                     {
-                --                         latte = u.lighten(colors.mantle, 0.70, colors.base)
-                --                     },
-                --                     u.darken(colors.surface0, 0.64, colors.base)
-                --                 ),
-                --             },
-                --         }
-                --     end,
-            }
-            require('catppuccin').setup(opts)
-            vim.cmd [[ colorscheme catppuccin ]]
-            -- vim.cmd [[ colorscheme kanagawa ]]
-        end,
-    },
-    {
-        "rebelot/kanagawa.nvim"
-    }
+	{
+		"catppuccin/nvim",
+		enabled = true,
+		priority = 1000,
+		lazy = false,
+		config = function()
+			local opts = {
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				term_color = true,
+				color_overrides = {
+					mocha = {
+						base = "#080A0F",
+						mantle = "#080A0F",
+						crust = "#bac2de",
+					},
+				},
+				custom_highlights = function(colors)
+					return {
+						["@function.builtin"] = { fg = colors.blue },
+					}
+				end,
+				-- higlight_overrides = {
+				--     all = function(colors)
+				--         return {
+				--             Comment = { fg = "#000000" },
+				--         }
+				--     end,
+				-- },
+				--     transparent_background = true,
+				--     custom_highlights = function(colors)
+				--         local u = require("catppuccin.utils.colors")
+				--         return {
+				--             CursorLine = {
+				--                 bg = u.vary_color(
+				--                     {
+				--                         latte = u.lighten(colors.mantle, 0.70, colors.base)
+				--                     },
+				--                     u.darken(colors.surface0, 0.64, colors.base)
+				--                 ),
+				--             },
+				--         }
+				--     end,
+			}
+			require("catppuccin").setup(opts)
+			vim.cmd([[ colorscheme catppuccin ]])
+			-- vim.cmd [[ colorscheme kanagawa ]]
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+	},
 }
